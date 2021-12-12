5db592c8436db5e0e5f0082a8ebd4fa9de4f4899
sandwich <122079260@qq.com>
2021-12-12 20:57:25 +0800

    :smile:submit oop - property

---
+++ OOP/property.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File              : property.py
+# Author            : sandwich <122079260@qq.com>
+# Date              : 2021-12-06 20:51:10
+# Last Modified Date: 2021-12-08 16:42:58
+# Last Modified By  : sandwich <122079260@qq.com>
+
+
+class Student:
+    def __init__(self, name) -> None:
+        self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str):
+            self._name = value
+        else:
+            raise TypeError
+
+    @name.deleter
+    def name(self):
+        self._name = ""
+
+
+if __name__ == "__main__":
+    # 实现name getter
+    s = Student("sand")
+    # 实现name setter
+    print(s.name)
+    s.name = "leo"
+    print(s.name)
+    del s.name
+    print(s.name)
+    # 触发setter的异常
+    s.name = 123
