0898c995429b72248571bc4d40930b11a7817784
sandwich <122079260@qq.com>
2022-01-09 22:26:53 +0800

    submit cache decorator

---
+++ Closure/deco_cache.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File              : deco_cache.py
+# Author            : sandwich <122079260@qq.com>
+# Date              : 2021-12-31 14:10:38
+# Last Modified Date: 2021-12-31 19:10:14
+# Last Modified By  : sandwich <122079260@qq.com>
+
+from functools import cached_property
+
+if __name__ == "__main__":
+    sand = 'hello'
+    sand.count()
