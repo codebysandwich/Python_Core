652638dd0317031feb564f8fa5ed6178b9e002ea
sandwich <122079260@qq.com>
2022-01-10 15:41:50 +0800

    update kwargs

--- Notes/Foundation/args_collection.py
+++ Notes/Foundation/args_collection.py
@@ -3,7 +3,7 @@
 # File              : args_collection.py
 # Author            : sandwich <122079260@qq.com>
 # Date              : 2022-01-10 15:08:25
-# Last Modified Date: 2022-01-10 15:08:25
+# Last Modified Date: 2022-01-10 15:25:38
 # Last Modified By  : sandwich <122079260@qq.com>
 
 import numpy as np
@@ -22,8 +22,16 @@ def get_mean(*args):
         return np.nan
 
 
+def show_kwargs(**kwargs):
+    for k, v in kwargs.items():
+        print(f"{k}:  {v}")
+
+
 if __name__ == "__main__":
     print(get_mean())
     print(get_mean(3, 4, 9))
     print(get_mean(3, 4, 9, 12))
     print(get_mean(3, 4, 9, 12, 99))
+
+    print("======演示具名参数收集======")
+    show_kwargs(a=1, b="test", dicts={"name": "sand", "age": 20})
