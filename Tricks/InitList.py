ded5380bff1bdb7e2825b847bdd2b63d3be8895f
sandwich <122079260@qq.com>
2022-06-05 10:16:24 +0800

    submit list used in default args

---
+++ Tricks/InitList.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File              : InitList.py
+# Author            : sandwich <122079260@qq.com>
+# Date              : 2022-04-29 15:40:34
+# Last Modified Date: 2022-05-05 09:31:30
+# Last Modified By  : sandwich <122079260@qq.com>
+
+
+def append(item, lst=[]):
+    '''
+    初始化的lst会一直占用
+    '''
+    lst.append(item)
+    return lst
+
+
+if __name__ == "__main__":
+    print(append(1))
+    print(append(2))
+    l = [3]
+    print(append(99, l))
